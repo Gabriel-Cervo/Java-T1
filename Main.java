bafc74018e44332845cfc48adb7f734e20923c5c
Gabriel-Cervo <joaogabrieldouradocervo@gmail.com>
2021-04-10 16:53:18 -0300

    ATT spacing between Teste and :

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
         for (int i = 5; i <= 13; i++) {
             MinHeap lista = new MinHeap();
             LeitorCSV.importarCSV("teste" + i, lista);
-            System.out.println("Teste" + i + " : " + lista.howManyAreBalanced());
+            System.out.println("Teste" + i + ": " + lista.howManyAreBalanced());
         }
     }
 }
